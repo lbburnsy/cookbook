39be1f2e094fa7d236061c19de55010a2a07f1d5
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-21 17:16:13 -0400

    created navbar with its routes and the routes are working, also created some dummy text in the pages that i am rendering through routes for testing purposes

---
+++ src/components/SearchBox/searchBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchBox(){
+    return(
+        <div className="searchbox">
+            <h1>Search Box</h1>
+            
+
+        </div>
+    )
+}
+
+export default SearchBox;
