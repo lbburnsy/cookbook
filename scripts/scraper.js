fbd0168afefb3e9e896cb9b9de4f4a5c52e520a8
Luc Burns <burnsluc2@gmail.com>
2021-06-21 22:23:45 -0400

    seeding data, seed script, and scraper set up

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -1,7 +1,7 @@
 const recipeScraper = require("recipe-scraper");
 
 recipeScraper(
-  "https://www.kitchenstories.com/en/recipes/cinnamon-roll-pancakes-7f45"
+  "https://www.kitchenstories.com/en/recipes/cheesy-cauliflower-pizza-with-shaved-asparagus"
 )
   .then((recipe) => console.log(recipe))
   .catch((err) => console.err(err));
