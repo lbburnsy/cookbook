c1b4423b911f1cf300be660f9ce087941020ba0b
Luc Burns <burnsluc2@gmail.com>
2021-06-23 19:13:46 -0400

    Added email field in userSchema

--- models/user.js
+++ models/user.js
@@ -2,21 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  // Email
+  email: {
+    type: String,
+    required: true,
+  },
   // Username
   username: {
     type: String,
-    required: true
+    required: true,
   },
   // Password
   password: {
     type: String,
-    required: true
+    required: true,
   },
   // Amatuer, professional, etc
   abilityLevel: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
