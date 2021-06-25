525dff0016f58fbf6fadb45eef61cbc9b3e63f29
srad25 <suzanne.radlein@gmail.com>
2021-06-25 15:40:42 -0400

    adding recipe details page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import Home from "./pages/Home/home";
 import Recipes from "./pages/Recipes/recipes";
 import Signup from "./components/Signup/signup";
 import Login from "./pages/Login/login";
-import RecipeSearchResults from './pages/RecipeSearchResults/recipeSearchResults';
-import ProfilePage from "./pages/ProfilePage/profilePage"; 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Cookbook from "./pages/Cookbook/cookbook";
-import Addrecipe from "./pages/Addrecipe/addrecipe";
-import Favorite from "./pages/Favorite/favorite";
 
 
 function App() {
@@ -23,14 +18,7 @@ function App() {
           <Route path="/recipes" component= { Recipes }/>
           <Route path="/signup" component= { Signup }/>
           <Route path="/login" component= { Login }/>
-          <Route path="/profile" component= { ProfilePage }/> 
-          <Route path="/recipesearchresults" component= { RecipeSearchResults }/>
-
-
-{/* Sidebar */}
-          <Route path='/cookbook' component={Cookbook} />
-          <Route path='/addrecipe' component={Addrecipe} />
-          <Route path='/favorite' component={Favorite} />
+  
         </Switch>
       </div>
     </Router>
