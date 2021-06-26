380cc39a60961e4f80fc20ab2eb4da7aaa4362c7
Luc Burns <burnsluc2@gmail.com>
2021-06-26 12:14:19 -0400

    Recipes page render

--- client/src/pages/Recipes/recipes.js
+++ client/src/pages/Recipes/recipes.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./recipes.css";
+import API from "../../utils/API";
+import FoodCardContainer from "../../components/FoodCard/FoodCardContainer";
 
 function Recipes() {
-    return(
-        <div className="recipes">
-            <h1>Recipes Page</h1>
+  const [recipes, setRecipes] = useState([]);
 
-        </div>
+  useEffect(() => {
+    loadRecipes();
+  }, []);
 
-    )
+  function loadRecipes() {
+    API.getRecipes()
+      .then((res) => setRecipes(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className="recipes">
+      <h1>Recipes Page</h1>
+      <FoodCardContainer recipes={recipes} />
+    </div>
+  );
 }
 
-export default Recipes; 
+export default Recipes;
