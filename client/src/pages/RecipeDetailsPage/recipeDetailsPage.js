b9f5f16a3a94bdd7eedad79257b422b015d58197
srad25 <suzanne.radlein@gmail.com>
2021-06-26 14:48:51 -0400

    adding the body for recipe detail

--- client/src/pages/RecipeDetailsPage/recipeDetailsPage.js
+++ client/src/pages/RecipeDetailsPage/recipeDetailsPage.js
@@ -1,21 +1,22 @@
 import React from "react";
 import RecipeDetailSideBar from "../../components/RecipeDetailSideBar/recipeDetailSideBar";
-
+import RecipeDetailsBody from "../../components/RecipeDetailsBody/recipeDetailsBody";
+import "./recipeDetailsPage.css";
 function RecipeDetailsPage() {
     return (
-        <div className="container">
+        <div className="container mt-5">
             <div className="row">
                 <div className="col-lg-4">
-                 <img src="https://images.kitchenstories.io/communityImages/c0b4b1026931ea2b0659b264f87dfefc_d1878d04-5876-406c-93ef-94b5a55af7cd.jpg" className="img" alt="nutty bar"></img> 
+                  <img src="https://images.kitchenstories.io/communityImages/c0b4b1026931ea2b0659b264f87dfefc_d1878d04-5876-406c-93ef-94b5a55af7cd.jpg" className="img" alt="nutty bar"></img>  
                 <RecipeDetailSideBar/>    
                 </div>
                 <div className="col-lg-8">
-                    
+                <RecipeDetailsBody/> 
                 </div>
             </div>
 
         </div>
-    )
+    );
 }
 
 export default RecipeDetailsPage;
