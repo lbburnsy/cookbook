833345cc625374e0d0eeed5d4f516503e057768f
Brittany Plummer <brittanyplummer@gmail.com>
2021-07-03 12:43:08 -0400

    updated npm package to include bug fixes now search works on name ingredients cuisine catergory and is caseinsensitive in navbar

--- client/src/components/SearchBox/searchBox.js
+++ client/src/components/SearchBox/searchBox.js
@@ -1,9 +1,10 @@
 import React from "react";
 // import { propTypes } from "react-bootstrap/esm/Image";
-import SearchBar from 'react-js-search';
+import SearchBar from 'omega-react-js-search';
 import { useHistory } from "react-router-dom";
 
 import './searchBox.css'
+import Ingredients from "../../pages/Ingredients/ingredients";
 
 
 // had a problem with figuring out how this is filtered / search will be done looking at all values in objects
@@ -20,29 +21,33 @@ function SearchBox(props){
  
 
 }
+
 const resetText = () => {
   const form = document.getElementsByClassName("search-container");
   const input = (form[0].children[0]);
   input.value = "";
 }
+
   const onSubmit = (e) => {
     e.preventDefault();
     props.onText();
     resetText()
   }
+
     return(
         <form className="searchbox py-2" onSubmit= {onSubmit}>
 
            
                 <SearchBar 
                 id= "hello"
-                // caseInsensitive = {false}
+                caseInsensitive = {true}
                 // ^ this prop does not work
                 onSearchTextChange={props.onText}
                 // everytime you type a letter a change is triggred only in filter component
                 onSearchButtonClick={onSearch}
                 placeHolderText={"Search here..."}
                 data={props.recipes}
+                keys={["name", "ingredients", "cuisine", "category"]}
                 />
           
 
