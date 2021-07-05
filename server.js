2246d6d3f2d0c9946402b35ea3d1d39eca8a6b5e
Luc Burns <burnsluc2@gmail.com>
2021-07-05 17:45:54 -0400

    rebuild server.js

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
+const path = require("path");
 const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-// Define middleware
-app.use(express.urlencoded({ extended: true }));
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
+app.use(express.urlencoded({ extended: true }));
+
+app.use(compression());
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cookbook");
-
-// Start the API server
-app.listen(PORT, function () {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.list(PORT, () => console.log(`Now listening on PORT ${PORT}`));
 });
