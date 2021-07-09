4fa9a05b2d69937b466e5cdaf511e1fd13c414e5
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-07-08 21:09:11 -0400

    updated folder structure for deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const routesUrls = require("./routes/routes");
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"));
+
+
+app.use(express.json())
+app.use(cors())
+app.use("/app", routesUrls)
+app.listen(PORT, () =>  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`));
